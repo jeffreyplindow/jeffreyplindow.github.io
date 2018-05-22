c3ff0aecf2fabeb6d9ac9ad7f4e534239f565221
Jeffrey Lindow <35978964+jeffreyplindow@users.noreply.github.com>
2018-05-21 21:18:44 -0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "ae7R1aGdXEsC33OGlGF22f3Zs"
+consumer_secret = "jxHdLEeGVGGjY08hoDcSF5P70sKmnO09Lh537wsa6jOrmKA0Lv"
+access_token = "413505097-lGcpBoa1LnkdmtcNqmfPeWHBWwgnLIyu5sSLgRdW"
+access_token_secret = "Dt9Sx3mKeacKoSbs3TsenClNRf4aGahXiqum5w3dOP2Vl"
